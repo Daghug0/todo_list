e237bcb27b05a4570d360f4baee8b2187f4ec4dc
Daghug0 <hugdage@hotmail.fr>
2024-11-11 19:41:00 +0100

    begin implementation of link between collaborators and tasks collection

--- src/api/database_manager.py
+++ src/api/database_manager.py
@@ -37,29 +37,33 @@ class DataBaseManager:
     def write_task(self, query : dict) -> pymongo.cursor.CursorType:
         return self.tasks_collection.insert_one(query)
 
-    def modify_task(self, filter, updates : dict) -> pymongo.results.UpdateResult:
-        return self.tasks_collection.update_one(filter, updates)
+    def modify_task(self, task_id : str, updates : dict) -> pymongo.results.UpdateResult:
+        return self.tasks_collection.update_one(task_id, updates)
     
-    def delete_task(self, filter : dict) -> pymongo.cursor.CursorType:
-        return self.tasks_collection.delete_one(filter)
+    def delete_task(self, task_id : str) -> pymongo.cursor.CursorType:
+        return self.tasks_collection.delete_one(task_id)
 
+    #maybe redundant with read_tasks()
     def is_task_present(self, filter) -> bool:
         return (len(self.tasks_collection.find_one(filter))!=0)
 
+    #maybe useless
     def find_one_task(self,filter) -> pymongo.cursor.CursorType:
         return self.tasks_collection.find_one(filter)
     
-    def find_collaborator(self, collaborator_filter):
-        return self.collaborators_collection.find_one(collaborator_filter)
+    def get_collaborator_document(self, collaborator_id : str) -> dict:
+        return self.collaborators_collection.find_one({"_id": collaborator_id})
     
-    def is_collaborator_present(self, filter) -> bool:
-        return (len(self.collaborators_collection.find_one(filter))!=0)
+    def get_collaborator_id(self, collaborator_name : dict) -> str:
+        return self.collaborators_collection.find_one({"name": collaborator_name})["_id"]
     
-    def link_collaborator_to_task(self, task, collaborator_name):
-        if self.is_collaborator_present(collaborator_name):
-            collaborator_object = self.find_collaborator(collaborator_name)
-            return self.modify_task(task, {"collaborator" : collaborator_object["_id"]}) 
-        
+    def link_collaborator_to_task(self, task, collaborator_name : dict) -> pymongo.results.UpdateResult:
+        collaborator_id = self.get_collaborator_id(collaborator_name)
+        if collaborator_id != None:
+            return self.modify_task(task, {"collaborator" : collaborator_id})
+        else :
+            return None
+
     
 
         
